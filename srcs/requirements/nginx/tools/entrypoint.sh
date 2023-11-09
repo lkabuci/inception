aacfa47ad292ce34616f34804fd80dd023eef169
redone <elkaboussi@pm.me>
2023-11-09 12:10:47 +0100

    some changes

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# This entrypoint script will generate a self-signed certificates
+# for SSL and run the nginx daemon
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+CERTS_DIR="$SCRIPT_DIR/../conf/certs"
+mkdir -p $SCRIPT_DIR/../conf/certs
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_DIR/nginx.key -out $CERTS_DIR/nginx.crt -subj "/CN=localhost"
+
+nginx -g daemon off;
+
 # req \                                       # certificate request
 # -x509 \                                     # create a self-signed certificate rather than a certificate request.
 # -nodes \                                    # avoid encrypt the private keys with passphrase
@@ -8,10 +18,4 @@
 # -out certs/nginx.crt \                      # the file where the self-signed certificate will be saved.
 # -subj "/CN=localhost"                       # the subject of the certificate
 
-
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-CERTS_DIR="$SCRIPT_DIR/../conf/certs"
-
-mkdir -p $SCRIPT_DIR/../conf/certs
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_DIR/nginx.key -out $CERTS_DIR/nginx.crt -subj "/CN=localhost"
+# -g daemon off    - to keep running in the foreground while applying the config on global
