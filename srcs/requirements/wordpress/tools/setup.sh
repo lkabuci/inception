d563cdc73303e979ad822fbf0af50c3e8feedc6c
redone <elkaboussi@pm.me>
2023-11-26 02:08:32 +0100

    declutter wordpress dockerfile

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-SourceDir="/var/www/html"
 
-mkdir -p "$SourceDir" && cd "$SourceDir" || exit
-
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar wp
-mv wp /usr/local/bin/
-wp core download --allow-root
-
+cd /var/www/html
 mv wp-config-sample.php wp-config.php
 
-sed -i "s/database_name_here/$DATABASE/g" wp-config.php
-sed -i "s/username_here/$USERNAME/g"      wp-config.php
-sed -i "s/password_here/$PASSWORD/g"      wp-config.php
-sed -i "s/localhost/$HOSTNAME/g"          wp-config.php
+sed -i "s/database_name_here/$WORDPRESS_DB_NAME/1" wp-config.php
+sed -i "s/username_here/$WORDPRESS_DB_USER/1"      wp-config.php
+sed -i "s/password_here/$WORDPRESS_DB_PASS/1"      wp-config.php
+sed -i "s/localhost/$WORDPRESS_DB_HOST/1"          wp-config.php
