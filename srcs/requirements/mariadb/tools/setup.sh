4661143c32fd3de102e0a43c4033d8f0a19a06f2
redone <elkaboussi@pm.me>
2023-11-29 05:02:03 +0100

    small improvement in the nginx image

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -2,11 +2,16 @@
 
 service mariadb start
 
-# wait untill mariadb stated
-until timeout 1 bash -c "</dev/tcp/localhost/3306" > /dev/null 2>&1; do
-    echo "Waiting for mariadb database to start ..."
-    sleep 1
+# wait untill mariadb stated using netcat tool
+for ((i=1; i<=5; i++)); do
+    if nc -z localhost 3306 >/dev/null; then
+        echo "MariaDB database is up."
+        break
+    fi
+    echo "Waiting for MariaDB database to start ..."
+    sleep 5
 done
+
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
@@ -15,4 +20,4 @@ mariadb -e "FLUSH PRIVILEGES;"
 
 service mariadb stop
 
-mariadbd
+exec "$@"
