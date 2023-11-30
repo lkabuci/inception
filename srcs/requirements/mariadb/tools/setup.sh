8b0b60f08c8183aa072541da46d5caac279e44d0
redone <elkaboussi@pm.me>
2023-11-30 14:08:21 +0100

    mariadb: fix unable to find socket file

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sed -i 's/# port = 3306/port = 3306/'	/etc/mysql/mariadb.cnf
-sed -i 's/127.0.0.1/0.0.0.0/' 		/etc/mysql/mariadb.conf.d/50-server.cnf
+set -x
+
+sed -i 's/# port = 3306/port = 3306/' /etc/mysql/mariadb.cnf
+sed -i 's/127.0.0.1/0.0.0.0/'         /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mariadb start
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mariadb -e "CREATE DATABASE IF NOT EXISTS $WP_DB;"
+mariadb -e "CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS';"
+mariadb -e "GRANT ALL PRIVILEGES ON $WP_DB.* TO '$WP_USER'@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 
 service mariadb stop
