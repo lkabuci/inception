7ec5c7792cf51110f0c62055b67af246242a5bfe
redone <elkaboussi@pm.me>
2024-01-25 18:31:28 +0100

    simplify sed command and correct user_pass

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -2,20 +2,18 @@
 
 set -x
 
-while ! mariadb -h"$WP_HOST" -u"$WP_USER" -p"$WP_PASS" "$WP_DB" &> /dev/null
-
 chmod +x /usr/local/bin/wp
 
 mkdir -p /var/www/html/ && cd /var/www/html || exit 1
 
 wp core download --allow-root --force
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/1' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|1' /etc/php/7.4/fpm/pool.d/www.conf
 service php7.4-fpm start
 
 wp config create --allow-root --dbname="$WP_DB" --dbuser="$WP_USER" --dbpass="$WP_PASS" --dbhost="$WP_HOST"
 wp db create --allow-root
 wp core install --allow-root --url="$URL" --title="$TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASS" --admin_email="$ADMIN_MAIL"
-wp user create --allow-root "$WP_REGULAR_USER" "$WP_REGULAR_MAIL" --user-pass="$WP_REGULAR_PASS" --role=author
+wp user create --allow-root "$WP_REGULAR_USER" "$WP_REGULAR_MAIL" --user_pass="$WP_REGULAR_PASS" --role=author
 
 wp plugin install multiple-domain --activate --path="/var/www/html/" --allow-root
 
