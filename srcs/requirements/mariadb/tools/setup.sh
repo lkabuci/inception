0b8c8d209b6929964089bd8be520b988bc31bc71
redone <elkaboussi@pm.me>
2024-01-27 18:43:11 +0100

    add label metadata

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -7,7 +7,7 @@ sed -i 's/127.0.0.1/0.0.0.0/'         /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mariadb start
 
-mysqladmin -u root password $DB_ROOT_PASS
+mysqladmin -u root password "$DB_ROOT_PASS"
 mariadb -e "CREATE DATABASE IF NOT EXISTS $WP_DB;"
 mariadb -e "CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS';"
 mariadb -e "GRANT ALL PRIVILEGES ON $WP_DB.* TO '$WP_USER'@'%';"
