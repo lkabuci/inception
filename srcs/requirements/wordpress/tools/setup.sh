c1a968d65921929d8507649deec35af534c994b8
relkabou <elkaboussi@pm.me>
2024-02-09 10:40:31 +0100

    feat: Add environment variables to WordPress setup script
    - Added environment variables for database connection and WordPress configuration in `.env` file
    - Modified `setup.sh` script to use the environment variables for database setup and configuration

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -10,15 +10,15 @@ wp core download --allow-root --force
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|1' /etc/php/7.4/fpm/pool.d/www.conf
 service php7.4-fpm start
 
-wp config create --allow-root --dbname="$WP_DB" --dbuser="$WP_USER" --dbpass="$WP_PASS" --dbhost="$WP_HOST"
+wp config create --allow-root --dbname="$WP_DB" --dbuser="$WP_USER" --dbpass="$WP_PASS" --dbhost=172.18.0.2:3306
 wp db create --allow-root
 wp core install --allow-root --url="$URL" --title="$TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASS" --admin_email="$ADMIN_MAIL"
 wp user create --allow-root "$WP_REGULAR_USER" "$WP_REGULAR_MAIL" --user_pass="$WP_REGULAR_PASS" --role=author
 
 wp config set FS_METHOD direct --allow-root --path="/var/www/html/"
-wp config set WP_REDIS_HOST "$WP_REDIS_HOST" --add --allow-root --path="/var/www/html/"
-wp config set WP_REDIS_PORT "$WP_REDIS_PORT" --add --allow-root --path="/var/www/html/"
-wp config set WP_REDIS_DATABASE "$WP_REDIS_DATABASES" --add --allow-root --path="/var/www/html/"
+wp config set WP_REDIS_HOST 172.18.0.7 --add --allow-root --path="/var/www/html/"
+wp config set WP_REDIS_PORT 6379 --add --allow-root --path="/var/www/html/"
+wp config set WP_REDIS_DATABASE 1 --add --allow-root --path="/var/www/html/"
 wp plugin install redis-cache --activate --path="/var/www/html/" --allow-root
 wp redis enable --path="/var/www/html/" --allow-root
 
